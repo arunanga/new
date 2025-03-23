533457cd30dd999166a2cc3c8e978a946480b4e5
arunanga <51504361+arunanga@users.noreply.github.com>
2025-03-23 19:06:35 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import FarmHouse from './pages/FarmHouse';
+import SwimmingPool from './pages/SwimmingPool';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/farmhouse" element={<FarmHouse />} />
+            <Route path="/swimming-pool" element={<SwimmingPool />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
